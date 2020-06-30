6394b04482d41866dafaf9ba44da7e49afd155ce
Lucyna Aleksandra <lucyaleksa@gmail.com>
2020-06-30 09:23:34 -0400

    refactored all buttons

--- lib/login_button.dart
+++ lib/login_button.dart
@@ -12,33 +12,72 @@ class LoginButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => selectHomePage(context),
-      child: Container(
-        height: 48,
-        width: 330,
-        child: Material(
+    return SizedBox(
+      width: 250,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30),
-          shadowColor: Colors.teal[500],
-          color: Colors.teal[200],
-          elevation: 7,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed('/home_page');
-            },
-            child: Center(
+          side: BorderSide(
+            color: Colors.teal[400],
+            width: 2,
+          ),
+        ),
+        hoverColor: Colors.teal[500],
+        color: Colors.teal[400],
+        padding: EdgeInsets.only(top: 14.0, bottom: 14.0),
+        animationDuration: Duration(milliseconds: 500),
+        elevation: 5,
+        splashColor: Colors.teal[300],
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MyHomePage()),
+          );
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
               child: Text(
-                "Login".toUpperCase(),
+                'Login'.toUpperCase(),
                 style: TextStyle(
-                  color: Colors.white,
                   fontSize: 15,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
+    // return InkWell(
+    //   onTap: () => selectHomePage(context),
+    //   child: Container(
+    //     height: 48,
+    //     width: 330,
+    //     child: Material(
+    //       borderRadius: BorderRadius.circular(30),
+    //       shadowColor: Colors.teal[500],
+    //       color: Colors.teal[200],
+    //       elevation: 7,
+    //       child: GestureDetector(
+    //         onTap: () {
+    //           Navigator.of(context).pushNamed('/home_page');
+    //         },
+    //         child: Center(
+    //           child: Text(
+    //             "Login".toUpperCase(),
+    //             style: TextStyle(
+    //               color: Colors.white,
+    //               fontSize: 15,
+    //               fontWeight: FontWeight.bold,
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
--- lib/sign_up_button.dart
+++ lib/sign_up_button.dart
@@ -2,53 +2,52 @@ import 'package:flutter/material.dart';
 import './home_page.dart';
 
 class SignUpButton extends StatelessWidget {
-  void selectHomePage(BuildContext context) {
-    Navigator.of(context).push(
-      new MaterialPageRoute(builder: (context) {
-        return MyHomePage();
-      }),
-    );
-  }
+  // void selectHomePage(BuildContext context) {
+  //   Navigator.of(context).push(
+  //     new MaterialPageRoute(builder: (context) {
+  //       return MyHomePage();
+  //     }),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return new InkWell(
-      onTap: () => selectHomePage(context),
-      borderRadius: BorderRadius.circular(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 48,
-            width: 300,
-            child: FlatButton(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyHomePage()),
-                );
-              },
-              child: Material(
-                borderRadius: BorderRadius.circular(30),
-                shadowColor: Colors.teal[500],
-                color: Colors.teal[200],
-                elevation: 20,
-                child: Center(
-                  child: Text(
-                    "Sign Up".toUpperCase(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+    return new SizedBox(
+      width: 250,
+      // onTap: () => selectHomePage(context),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+          side: BorderSide(
+            color: Colors.teal[300],
+            width: 2,
+          ),
+        ),
+        hoverColor: Colors.teal[500],
+        color: Colors.teal[400],
+        padding: EdgeInsets.only(top: 14.0, bottom: 14.0),
+        animationDuration: Duration(milliseconds: 500),
+        elevation: 5,
+        splashColor: Colors.teal[300],
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MyHomePage()),
+          );
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Sign Up".toUpperCase(),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
